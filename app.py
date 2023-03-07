0cd495a8f0682aed2f29ecfb9393203fef0d6099
B V S YASWANTH <83590022+s-1-n-t-h@users.noreply.github.com>
2023-03-07 18:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def init():
     global model
     
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    model = pipeline('summarization', model="google/pegasus-cnn_dailymail", framework='pt',
+                     device=device, use_auth_token='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')
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.

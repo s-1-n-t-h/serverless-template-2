af4376ee074fdb3eacc890d9479ca45634556fc5
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-03-08 20:17:48 +0530

    Revert "Update download.py"
    This reverts commit f66fb5f3d83266fa2cccd9e5e66c346605dd7a71.

--- download.py
+++ download.py
@@ -4,16 +4,10 @@
 # In this example: A Huggingface BERT model
 
 from transformers import pipeline
-from transformers import AutoTokenizer, pipeline
-from optimum.onnxruntime import ORTModelForSeq2SeqLM
-
 
 def download_model():
-    model = ORTModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn", from_transformers=True)
-    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
     # do a dry run of loading the huggingface model, which will download weights
-    #pipeline('summarization', model="google/pegasus-cnn_dailymail", framework='pt',use_auth_token='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')
-    pipeline("summarization", model=model, tokenizer=tokenizer)
+    pipeline('summarization', model="google/pegasus-cnn_dailymail", framework='pt',use_auth_token='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')
 
 if __name__ == "__main__":
     download_model()

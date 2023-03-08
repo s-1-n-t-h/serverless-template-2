68cf63628f85b9abbfd7f6b920866acb75a4a911
B V S YASWANTH <83590022+s-1-n-t-h@users.noreply.github.com>
2023-03-08 20:23:38 +0530

    Update download.py

--- download.py
+++ download.py
@@ -3,11 +3,11 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import pipeline
+from optimum.pipelines import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('summarization', model="google/pegasus-cnn_dailymail", framework='pt',use_auth_token='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')
+    pipeline('summarization', model="s-1-n-t-h/bart-cnn-optimised", framework='pt',use_auth_token='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')
 
 if __name__ == "__main__":
     download_model()
